ba27a96e879c5067237b7b017102c91c63068a3d
ashok <ashok@gmail.com>
2023-10-01 20:42:29 +0530

    commit

--- Git-TL/src/com/nt/dao/Xyz.java
+++ Git-TL/src/com/nt/dao/Xyz.java
@@ -3,11 +3,9 @@ package com.nt.dao;
 public class Xyz {
 	
 	public void m1() {
-<<<<<<< HEAD
-		System.out.println("Xyz-m1() method-testing");
-=======
+
 		System.out.println("Xyz-m1() method-learning");
->>>>>>> branch 'master' of https://github.com/ashoksankargannarapu/FullStackRepo.git
+
 	}
 
 }
